9c39bc73b343542490c66d2cee47dc82ed286b1d
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-08-20 10:25:44 -0700

    Test routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NavbarComponent } from "./navbar/navbar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: NavbarComponent
+  },
+  {
+    path: "test",
+    component: NavbarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
